100c4ac586592bd14e1d59c17299ddd4f25d5bb8
Eldrick Millares <eldrick@stanford.edu>
2021-05-26 10:53:53 -0700

    Adds wakey_wakey design files

--- verilog/rtl/user_proj_example.v
+++ verilog/rtl/user_proj_example.v
@@ -13,159 +13,160 @@
 // limitations under the License.
 // SPDX-License-Identifier: Apache-2.0
 
-`default_nettype none
+// `default_nettype none
+
+`ifndef MPRJ_IO_PADS
+    `define MPRJ_IO_PADS 38
+`endif
+
 /*
  *-------------------------------------------------------------
  *
  * user_proj_example
  *
- * This is an example of a (trivially simple) user project,
- * showing how the user project can connect to the logic
- * analyzer, the wishbone bus, and the I/O pads.
- *
- * This project generates an integer count, which is output
- * on the user area GPIO pads (digital output only).  The
- * wishbone connection allows the project to be controlled
- * (start and stop) from the management SoC program.
- *
- * See the testbenches in directory "mprj_counter" for the
- * example programs that drive this user project.  The three
- * testbenches are "io_ports", "la_test1", and "la_test2".
- *
+ * Instatiates WakeyWakey in user_proj_example for Caravel integration
  *-------------------------------------------------------------
  */
 
-module user_proj_example #(
-    parameter BITS = 32
-)(
+module user_proj_example (
 `ifdef USE_POWER_PINS
-    inout vdda1,	// User area 1 3.3V supply
-    inout vdda2,	// User area 2 3.3V supply
-    inout vssa1,	// User area 1 analog ground
-    inout vssa2,	// User area 2 analog ground
+    // inout vdda1,	// User area 1 3.3V supply
+    // inout vdda2,	// User area 2 3.3V supply
+    // inout vssa1,	// User area 1 analog ground
+    // inout vssa2,	// User area 2 analog ground
     inout vccd1,	// User area 1 1.8V supply
     inout vccd2,	// User area 2 1.8v supply
     inout vssd1,	// User area 1 digital ground
     inout vssd2,	// User area 2 digital ground
 `endif
 
+    input wire user_clock2,
+
     // Wishbone Slave ports (WB MI A)
-    input wb_clk_i,
-    input wb_rst_i,
-    input wbs_stb_i,
-    input wbs_cyc_i,
-    input wbs_we_i,
-    input [3:0] wbs_sel_i,
-    input [31:0] wbs_dat_i,
-    input [31:0] wbs_adr_i,
-    output wbs_ack_o,
-    output [31:0] wbs_dat_o,
+    input wire wb_clk_i,
+    input wire wb_rst_i,
+    input wire wbs_stb_i,
+    input wire wbs_cyc_i,
+    input wire wbs_we_i,
+    input wire [3:0] wbs_sel_i,
+    input wire [31:0] wbs_dat_i,
+    input wire [31:0] wbs_adr_i,
+    output wire wbs_ack_o,
+    output wire [31:0] wbs_dat_o,
 
     // Logic Analyzer Signals
-    input  [127:0] la_data_in,
-    output [127:0] la_data_out,
-    input  [127:0] la_oenb,
+    input  wire [127:0] la_data_in,
+    output wire [127:0] la_data_out,
+    input  wire [127:0] la_oenb,
 
     // IOs
-    input  [`MPRJ_IO_PADS-1:0] io_in,
-    output [`MPRJ_IO_PADS-1:0] io_out,
-    output [`MPRJ_IO_PADS-1:0] io_oeb,
+    input  wire [`MPRJ_IO_PADS-1:0] io_in,
+    output wire [`MPRJ_IO_PADS-1:0] io_out,
+    output wire [`MPRJ_IO_PADS-1:0] io_oeb,
 
     // IRQ
-    output [2:0] irq
+    output wire [2:0] irq
 );
-    wire clk;
-    wire rst;
 
-    wire [`MPRJ_IO_PADS-1:0] io_in;
-    wire [`MPRJ_IO_PADS-1:0] io_out;
-    wire [`MPRJ_IO_PADS-1:0] io_oeb;
+    // =========================================================================
+    // Wakey Wakey External I/O Declarations
+    // =========================================================================
+    wire wake;
+    `ifndef COCOTB_SIM
+    wire pdm_data;
+    wire pdm_clk;
+    `endif
+    wire vad;
 
-    wire [31:0] rdata; 
-    wire [31:0] wdata;
-    wire [BITS-1:0] count;
+    // =========================================================================
+    // Wakey Wakey Inputs
+    // =========================================================================
+    `ifdef COCOTB_SIM
+    localparam DFE_OUTPUT_BW = 8;
+    wire [DFE_OUTPUT_BW - 1 : 0] dfe_data = io_in[DFE_OUTPUT_BW - 1 : 0];  // 7-0
+    wire dfe_valid                        = io_in[DFE_OUTPUT_BW];  // 8
+    `else
+    assign pdm_data = io_in[36];
+    `endif
+    assign vad = io_in[34];
 
-    wire valid;
-    wire [3:0] wstrb;
-    wire [31:0] la_write;
+    // =========================================================================
+    // Wakey Wakey Instantiation
+    // =========================================================================
+    wakey_wakey wakey_wakey_inst (
+        // clock and reset
+        .clk_i(wb_clk_i),
+        .rst_n_i(~wb_rst_i),
 
-    // WB MI A
-    assign valid = wbs_cyc_i && wbs_stb_i; 
-    assign wstrb = wbs_sel_i & {4{wbs_we_i}};
-    assign wbs_dat_o = rdata;
-    assign wdata = wbs_dat_i;
+        // wishbone slave ports (wb mi a)
+        .wbs_stb_i(wbs_stb_i),
+        .wbs_cyc_i(wbs_cyc_i),
+        .wbs_we_i(wbs_we_i),
+        .wbs_sel_i(wbs_sel_i),
+        .wbs_dat_i(wbs_dat_i),
+        .wbs_adr_i(wbs_adr_i),
+        .wbs_ack_o(wbs_ack_o),
+        .wbs_dat_o(wbs_dat_o),
 
-    // IO
-    assign io_out = count;
-    assign io_oeb = {(`MPRJ_IO_PADS-1){rst}};
+        // microphone i/o
+        `ifdef COCOTB_SIM
+        .dfe_data(dfe_data),  // bypass DFE for test bench
+        .dfe_valid(dfe_valid),
+        `else
+        .pdm_data_i(pdm_data),
+        .pdm_clk_o(pdm_clk),
+        `endif
+        .vad_i(vad),
 
-    // IRQ
-    assign irq = 3'b000;	// Unused
-
-    // LA
-    assign la_data_out = {{(127-BITS){1'b0}}, count};
-    // Assuming LA probes [63:32] are for controlling the count register  
-    assign la_write = ~la_oenb[63:32] & ~{BITS{valid}};
-    // Assuming LA probes [65:64] are for controlling the count clk & reset  
-    assign clk = (~la_oenb[64]) ? la_data_in[64]: wb_clk_i;
-    assign rst = (~la_oenb[65]) ? la_data_in[65]: wb_rst_i;
-
-    counter #(
-        .BITS(BITS)
-    ) counter(
-        .clk(clk),
-        .reset(rst),
-        .ready(wbs_ack_o),
-        .valid(valid),
-        .rdata(rdata),
-        .wdata(wbs_dat_i),
-        .wstrb(wstrb),
-        .la_write(la_write),
-        .la_input(la_data_in[63:32]),
-        .count(count)
+        // wake output
+        .wake_o(wake)
     );
 
-endmodule
+    // =========================================================================
+    // Pin Directions (Output Enable)
+    // =========================================================================
+    assign io_oeb[37] = 1'b0;       // wake_o
+    assign io_oeb[36] = 1'b1;       // pdm_data_i
+    assign io_oeb[35] = 1'b0;       // pdm_clk_o
+    assign io_oeb[34] = 1'b1;       // vad_i
+    assign io_oeb[33:0] = 34'b1;    // unused, except by test bench
 
-module counter #(
-    parameter BITS = 32
-)(
-    input clk,
-    input reset,
-    input valid,
-    input [3:0] wstrb,
-    input [BITS-1:0] wdata,
-    input [BITS-1:0] la_write,
-    input [BITS-1:0] la_input,
-    output ready,
-    output [BITS-1:0] rdata,
-    output [BITS-1:0] count
-);
-    reg ready;
-    reg [BITS-1:0] count;
-    reg [BITS-1:0] rdata;
+    // =========================================================================
+    // Wakey Wakey Outputs
+    // =========================================================================
+    assign io_out[37] = wake;       // wake_o
+    assign io_out[36] = 1'b0;       // pdm_data_i
+    `ifdef COCOTB_SIM
+    assign io_out[35] = 1'b0;       // no pdm clock for test bench
+    `else
+    assign io_out[35] = pdm_clk;    // pdm_clk_o
+    `endif
+    assign io_out[34] = 1'b0;       // vad_i
+    assign io_out[33:0] = 34'b0;    // unused
 
-    always @(posedge clk) begin
-        if (reset) begin
-            count <= 0;
-            ready <= 0;
-        end else begin
-            ready <= 1'b0;
-            if (~|la_write) begin
-                count <= count + 1;
-            end
-            if (valid && !ready) begin
-                ready <= 1'b1;
-                rdata <= count;
-                if (wstrb[0]) count[7:0]   <= wdata[7:0];
-                if (wstrb[1]) count[15:8]  <= wdata[15:8];
-                if (wstrb[2]) count[23:16] <= wdata[23:16];
-                if (wstrb[3]) count[31:24] <= wdata[31:24];
-            end else if (|la_write) begin
-                count <= la_write & la_input;
-            end
-        end
+
+    // =========================================================================
+    // IRQ
+    // =========================================================================
+    assign irq = 3'b000;            // unused
+
+    // =========================================================================
+    // Logic Analyzer Outputs
+    // =========================================================================
+    assign la_data_out = 128'b0;    // unused
+
+    // =========================================================================
+    // Simulation Only Waveform Dump (.vcd export)
+    // =========================================================================
+    `ifdef COCOTB_SIM
+    `ifndef SCANNED
+    `define SCANNED
+    initial begin
+      $dumpfile ("wave.vcd");
+      $dumpvars (0, user_proj_example);
+      #1;
     end
+    `endif
+    `endif
 
 endmodule
-`default_nettype wire
